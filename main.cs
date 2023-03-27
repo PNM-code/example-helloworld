d03566dbf8cbddfd228ebdb49b600907ca0ac9c8
PNM-code <129080589+PNM-code@users.noreply.github.com>
2023-03-27 22:03:40 +0700

    Hello world in c#

---
+++ main.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace HelloWorld
+ {
+   public class Program
+   {
+     public static void Main(string[] args)
+     {
+       //Your code goes here
+       Console.WriteLine("Hello world");
+     }
+   }
+ }
