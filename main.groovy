e17cc8f4799221a72180bd4d4e8f0a878991da76
PNM-code <129080589+PNM-code@users.noreply.github.com>
2023-03-28 12:27:29 +0700

    Create main.groovy

---
+++ main.groovy
@@ -0,0 +1,10 @@
+//Groovy Version: 2.5.4
+//do not place package here, no need
+//main method must be in class Dcoder
+
+import java.io.File
+class Dcoder {
+  static void main(String[] args) {
+    println("Hello world");
+  }
+}
