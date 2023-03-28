18c914789a6a37c8155d0c578eac3a73865b5647
PNM-code <129080589+PNM-code@users.noreply.github.com>
2023-03-28 12:36:28 +0700

    Create main.m

---
+++ main.m
@@ -0,0 +1,9 @@
+#import <Foundation/Foundation.h>
+ // Compiler version gcc 6.3.0
+
+ int main (int argc, const char * argv[])
+ {
+   //NSLog (@"Hello world");
+   printf("Hello world");
+   return 0;
+ }
